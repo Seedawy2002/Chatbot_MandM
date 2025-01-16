71b81b6743bc9335ba41b029e95216a24971b5ed
Mariam Elseedawy <67360172+Seedawy2002@users.noreply.github.com>
2025-01-16 08:14:33 +0200

    Update rag_chain.py

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -8,6 +8,8 @@ from langchain.memory import ConversationSummaryBufferMemory
 import re
 
 os.environ.get("GROQ_API_KEY", "PINECONE_API_KEY")
+os.environ["GROQ_API_KEY"] =  "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"
+os.environ["PINECONE_API_KEY"] = "pcsk_5WCgdB_QmWFymLpXWR9CFEaXsPggzWS83xBiBiLmDvWX8RMPdcT6G2x6Wa2p9LEQCvX
 
 class RagChain:
     def __init__(self, index_name="new", embedding_model="BAAI/bge-base-en-v1.5", model_name="llama-3.3-70b-versatile", max_token_limit=1024):
